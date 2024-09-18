f305938c14a2344475c042a66b9ef75b4eac0855
Vincent Joy <vincentjoyhere@gmail.com>
2024-09-18 16:17:21 +0530

    created the UserProfileManager

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email adsress')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, name, password):
+        """Create and save a new super user with given details"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True # This is automatically create by PermissionsMixin
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
