cd8b69e5fef39bac37d721bb05fa9c05a44b1421
Vincent Joy <vincentjoyhere@gmail.com>
2024-09-20 12:06:15 +0530

    creted the UserProfileViewSet and serializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+
 from profiles_api import serializers
+from profiles_api import models
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -90,3 +92,9 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing of an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
